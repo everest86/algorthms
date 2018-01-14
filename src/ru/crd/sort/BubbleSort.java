59aebc08ec79c0cb58b19876e476804582ba3318
Roman Chaptykov <everest86@bk.ru>
2018-01-14 18:07:51 +0700

    add body method

--- src/ru/crd/sort/BubbleSort.java
+++ src/ru/crd/sort/BubbleSort.java
@@ -4,24 +4,29 @@ package ru.crd.sort;
  * Created by user on 14.01.2018.
  */
 public class BubbleSort {
-    private static int[] a={5,4,3,2,9};
+    private static int[] a = {5, 4, 3, 2, 9};
 
-    private static void print(){
+    private static void print() {
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
     }
-    public static void main(String[] args) {
+
+    private static void sort() {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                if(a[j]>a[i]){
-                    int t=a[i];
-                    a[i]=a[j];
-                    a[j]=t;
+                if (a[j] > a[i]) {
+                    int t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
                 }
             }
         }
+    }
 
+    public static void main(String[] args) {
+
+        sort();
         print();
     }
 }

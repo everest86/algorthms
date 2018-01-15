eeb386aa7af0dbe2409ec8f87480f568393944be
Roman Chaptykov <everest86@bk.ru>
2018-01-15 07:11:26 +0700

    quicksort

--- src/ru/crd/sort/SortClass.java
+++ src/ru/crd/sort/SortClass.java
@@ -7,7 +7,7 @@ public class SortClass {
     private static int[] a = {5, 4, 8, 2, 6, 3};
 
     public static void main(String[] args) {
-        Sort s = new ShellSortClass(a);
+        Sort s = new QuickSortClass(a);
         s.sort();
         s.print();
     }
@@ -109,7 +109,7 @@ class ShellSortClass implements Sort {
 
     @Override
     public void sort() {
-        int h = 0;
+        int h = 1;
         while (h <= a.length / 3) h = h * 3 + 1;
         while (h > 0) {
             for (int i = h; i < a.length; i++) {
@@ -121,6 +121,44 @@ class ShellSortClass implements Sort {
                 h /= 3;
             }
         }
+    }
+
+    @Override
+    public void print() {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+
+class QuickSortClass implements Sort {
+    private int[] a;
+
+    public QuickSortClass(int[] a) {
+        this.a = a;
+    }
+
+    @Override
+    public void sort() {
+        innerSort(a,0,a.length-1);
+    }
+
+    private void innerSort(int[]a,int l, int r){
+        int m = a[l+(r-l)/2];
+        int i=l,j=r;
+        while(i<=j){
+            while(a[j]>m)j--;
+            while (a[i]<m)i++;
+            if(i<=j){
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+                i++;
+                j--;
+            }
+        }
+        if(j>l)innerSort(a,l,j);
+        if(i<r)innerSort(a,i,r);
     }
 
     @Override
